c85019bea62552ab7c4cb146e3e792f0410b6db9
Jonas Hietala <mail@jonashietala.se>
2015-04-28 11:44:32 +0200

    products -> items

--- report/src/eswc.py
+++ report/src/eswc.py
@@ -7,24 +7,24 @@ class Eswc():
         parser.add_argument('file', metavar='eswc_training_file', type=str, help='The training file')
 
     def load(self, args):
-        """Load users and products."""
+        """Load users and items."""
 
-        products = {}
+        items = {}
         users = {}
 
         # Each line is composed by: userID \t itemID
         with open(args.file) as f:
             for line in f:
                 s = line.rstrip().split('\t')
-                user_id, product_id = s[0], s[1]
+                user_id, item_id = s[0], s[1]
 
-                if not product_id in products:
-                    products[product_id] = Product(product_id)
+                if not item_id in items:
+                    items[item_id] = Item(item_id)
 
                 if not user_id in users:
                     users[user_id] = User(user_id)
 
-                users[user_id].add_history(product_id, 1)
+                users[user_id].add_history(item_id, 1)
 
-        return users, products
+        return users, items
 
